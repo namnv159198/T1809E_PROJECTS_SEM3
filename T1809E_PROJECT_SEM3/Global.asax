e1480ea9b2d3860ac714a39d4737519db6fffc08
nam159198 <namkun159198@gmail.com>
2020-06-16 18:16:16 +0700

    new project

---
+++ T1809E_PROJECT_SEM3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T1809E_PROJECT_SEM3.MvcApplication" Language="C#" %>
---
+++ T1809E_PROJECT_SEM3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T1809E_PROJECT_SEM3.Startup))]
+namespace T1809E_PROJECT_SEM3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
