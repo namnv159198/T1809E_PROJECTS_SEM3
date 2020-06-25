d3a0b7b4e67fe8864ce8a56f9d4a456212b14a4c
xzoj <xzoj@gitlap.com>
2020-06-25 20:31:40 +0700

    Update tinh price ship trong view create order

--- T1809E_PROJECT_SEM3/Models/Order.cs
+++ T1809E_PROJECT_SEM3/Models/Order.cs
@@ -67,4 +67,26 @@ namespace T1809E_PROJECT_SEM3.Models
         public static object StatusEnum { get; internal set; }
        
     }
+    public class calPrice
+    {
+        public string ServiceId { get; set; }
+
+        [ForeignKey("ServiceId")]
+        public virtual Service Service { get; set; }
+        
+        [Required]
+        [Range(0, double.MaxValue)]
+        public Double Distance { get; set; }
+        
+        [Required]
+        [Range(0, double.MaxValue)]
+        public Double Weight { get; set; }
+        
+        
+        public Double PriceShip { get; set; }
+
+        public int TypeItemId { get; set; }
+        [ForeignKey("TypeItemId")]
+        public virtual TypeItem TypeItem { get; set; }
+    }
 }
