83a0d3417fd17bae2fffe6d4e6ee97c197ed2a14
TimmocEdoc <fraggler131@gmail.com>
2020-06-25 23:25:29 +0700

    Cho list office vao trong client

--- T1809E_PROJECT_SEM3/Controllers/ClientController.cs
+++ T1809E_PROJECT_SEM3/Controllers/ClientController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using T1809E_PROJECT_SEM3.Models;
 
 namespace T1809E_PROJECT_SEM3.Controllers
 {
     public class ClientController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Client
         public ActionResult Index()
         {
-            return View();
+            ClientViewModel client = new ClientViewModel();
+            client.Services = db.Services.AsEnumerable().ToList();
+            client.Offices = db.Offices.AsEnumerable().ToList();
+            return View(client);
         }
     }
 }
