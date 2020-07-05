b034480dac142198c708d26a7e96f75ef45ac360
TimmocEdoc <fraggler131@gmail.com>
2020-07-05 10:43:32 +0700

    add chart cho admin index

--- T1809E_PROJECT_SEM3/Controllers/AdminController.cs
+++ T1809E_PROJECT_SEM3/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,11 +13,14 @@ namespace T1809E_PROJECT_SEM3.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult Index(int? page, int? pageSize)
         {
-            ViewBag.TotalRevenuDay = db.Orders.AsEnumerable().Where(x => x.CreateAt.Value.ToString("MM/dd/yyyy") == DateTime.Now.ToString("MM/dd/yyyy")).Sum(x => x.PriceShip);
+            ViewBag.TotalReven = db.Orders.AsEnumerable().Where(x => x.CreateAt.Value.ToString("MM/dd/yyyy") == DateTime.Now.ToString("MM/dd/yyyy")).Sum(x => x.PriceShip);
             ViewBag.TotalOrderDay = db.Orders.AsEnumerable().Count(x => x.CreateAt.Value.ToString("MM/dd/yyyy") == DateTime.Now.ToString("MM/dd/yyyy"));
-            return View();
+            var dailyOrders = db.Orders.AsEnumerable().Where(o => o.CreateAt.Value.Day == DateTime.Now.Day).OrderBy(o => o.Status);
+            int defaSize = (pageSize ?? 5);
+            int pageNumber = (page ?? 1);
+            return View(dailyOrders.ToPagedList(pageNumber, defaSize));
         }
     }
 }
